8148015de0dbede50635751fb028265f0910d15f
Mehtab Alam <49854015+alammehtab@users.noreply.github.com>
2021-11-22 18:19:44 +0500

    Update appReducer.js

--- src/store/reducers/appReducer.js
+++ src/store/reducers/appReducer.js
@@ -1,14 +1,16 @@
 const initialState = {
   appName: "chat app",
-  appVersion: '10'
+  appVersion: "10"
 };
 
 const AppReducer = (state = initialState, action) => {
   switch (action.type) {
     case "CHANGEAPPNAME":
-      return { ...state, appName: action.appName };
-    case 'CHANGEAPPVERSION':
-      return{...state,appVersion:action.appVersion}
+      return { ...state, appName: action.payload };
+    case "CHANGEAPPVERSION":
+      return { ...state, appVersion: action.payload };
+    case 'CLEARDATA':
+      return initialState
     default:
       return state;
   }
